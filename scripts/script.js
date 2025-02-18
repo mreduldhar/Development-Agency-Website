110be7e24b4e956550b974252dcdf9d4577dbd7b
Mredul kanthy Dhar <72749608+mreduldhar@users.noreply.github.com>
2025-02-19 02:34:26 +0600

    mobile navigation bar completed

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  navDialog.classList.toggle("active");
+
+  // Show/hide hamburger button
+  if (navDialog.classList.contains("active")) {
+    hamburger.style.display = "none";
+  } else {
+    hamburger.style.display = "flex";
+  }
+}
