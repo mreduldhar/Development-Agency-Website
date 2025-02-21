18bfc791b93c2bd30bf4b3986d24e439ebb63971
Mredul kanthy Dhar <72749608+mreduldhar@users.noreply.github.com>
2025-02-21 21:58:35 +0600

    faq section complete

--- scripts/script.js
+++ scripts/script.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
   // width of a single card
   function getCardWidth() {
     const card = document.querySelector(".card-item");
-    return card ? card.offsetWidth + 10 : 300; // Default to 300px if no card exists
+    return card ? card.offsetWidth + 10 : 300;
   }
 
   // handle scrolling function
@@ -57,5 +57,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
   prevButtons.forEach((button) => {
     button.addEventListener("click", () => scrollGallery(-1));
+  });
+});
+
+// Faq Section
+document.querySelectorAll(".faq-question").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".faq-question.active").forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("active");
+        const otherArrow = otherItem.querySelector(".arrow");
+        otherArrow.classList.remove("rotate");
+        const otherAnswer = otherItem.nextElementSibling;
+        otherAnswer.style.display = "none";
+      }
+    });
+
+    item.classList.toggle("active");
+    const arrow = item.querySelector(".arrow");
+    arrow.classList.toggle("rotate");
+
+    let answer = item.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
   });
 });
